8b51be812555a7b9cb070445c9e1013c5f9059ba
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-02 11:39:42 -0700

    move the array.map() method out of the return statement

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import React from "react"
-//import Checkboxes from "./components/Checkboxes"
+import Checkboxes2 from "./components/Checkboxes2";
+//import Checkboxes2 from "./components/Checkboxes2"
 import Todolist from "./components/Todolist"
 
 function App() {
   console.log(Todolist)
+
+  const newTodoList= Todolist.map(todo => {
+    key={todo.id} task={todo.task} requiresConcentration = {todo.requiresConcentration}
+  });
+
   return (
+
     <div className="App">
-      <p>Hi Universe</p>
-      {Todolist.map(todo => { <p key={todo.id}></p>
-        return <div>{todo.task}</div>
-      })}
+      <p>To Do's</p>
+      
+        return <label>{todo.task}</label>
+      
+      
     </div>
   );
 }
---
+++ src/components/Checkboxes2.js
@@ -0,0 +1,14 @@
+//import Todolist from "./components/Todolist"
+import React from "react"
+
+function Checkboxes2(props) {
+    return (
+        <div className="ToDos-Div">
+         
+            <input type="checkbox" ></input> 
+           
+        </div>
+        )
+    }
+   
+    export default Checkboxes2;
