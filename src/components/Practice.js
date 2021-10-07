98a5b185c17c5e2782de320d591a3afc11d7a01f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-10-06 17:12:36 -0700

    created a method on component, commented it out

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -4,16 +4,24 @@ class Practice extends React.Component {
     
     state = {value: true};
     //{console.log(this.state.value)};
+   
+    //conditional rendering
+    //create a method on the class which renders??
+    //{console.log(this.state.value)}()=> this.state.value = false
 
     render() {
           
+        // renderP() {
+        //     <p>You are currently logged out</p>
+
+        // }
             return (
                 <div>
                     <p>🧜🏿‍♀️</p>
                     <p>!</p>
                     <p>You are currently logged in</p>
-                    <button onClick={()=> Object.assign(this.state.value,{value: false}) }>Hi</button>
-                    {console.log(this.state.value)}
+                    <button onClick={()=> alert('hi')}>Hi</button>
+                
                 </div>
             )
         }
